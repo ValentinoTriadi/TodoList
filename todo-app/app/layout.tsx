4ee7f9894f58f131cd80be1d70f6fc41760af9f0
ValentinoTriadi <valent122016@gmail.com>
2024-06-23 15:12:35 +0700

    feat: init next app

---
+++ todo-app/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans", });
+
+export const metadata: Metadata = {
+  title: "Todo List",
+  description: "A Todo List Application",
+  manifest: "/manifest.json",
+  
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ todo-app/next.config.mjs
@@ -0,0 +1,22 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  register: true,
+  skipWaiting: true,
+  swcMinify: true,
+  // disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+export default withPWA(nextConfig);
