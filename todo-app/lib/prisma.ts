e54c13ed877d7839344c190c6aa6922e391d0c28
ValentinoTriadi <valent122016@gmail.com>
2024-06-24 00:12:06 +0700

    fix: prisma db on production

--- todo-app/lib/prisma.ts
+++ todo-app/lib/prisma.ts
@@ -1,16 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-var global = globalThis as any;
+declare global {
+  var prisma: PrismaClient;
+}   
 
-let prisma : PrismaClient;
+export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma ) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
